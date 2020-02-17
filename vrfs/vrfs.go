a4689d33ee5a479f239e1786a146ecb716a734c2
jason <jason@jasons-MacBook-Pro.local>
2020-02-17 18:03:40 +0800

    changed vrf

--- vrfs/vrfs.go
+++ vrfs/vrfs.go
@@ -5,8 +5,13 @@ import (
   _ "io/ioutil"
   _ "os"
   _ "path/filepath"
+  "botag/vrf/p256"
 )
 
 
 func GenerateKey() {
+  sk, pk := p256.GenerateKey()
+  fmt.Println(sk.Private())
+  fmt.Println(pk)
+
 }
